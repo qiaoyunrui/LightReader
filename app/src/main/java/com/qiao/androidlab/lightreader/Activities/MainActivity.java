6c2930c2e4b37f7af02d57e03333eaafe63069ec
qiaoyunrui <2546521583@qq.com>
2015-11-28 22:33:08 +0800

    一半的界面

--- app/src/main/java/com/qiao/androidlab/lightreader/Activities/MainActivity.java
+++ app/src/main/java/com/qiao/androidlab/lightreader/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.qiao.androidlab.lightreader;
+package com.qiao.androidlab.lightreader.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.qiao.androidlab.lightreader.R;
 
 public class MainActivity extends AppCompatActivity {
 
