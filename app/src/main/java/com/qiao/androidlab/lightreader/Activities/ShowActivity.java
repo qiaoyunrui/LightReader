efbde87ed72517eed3b55d0f5a774acda9b10f71
qiaoyunrui <2546521583@qq.com>
2015-12-01 22:37:25 +0800

    2015.12.1  little

---
+++ app/src/main/java/com/qiao/androidlab/lightreader/Activities/ShowActivity.java
@@ -0,0 +1,22 @@
+package com.qiao.androidlab.lightreader.Activities;
+
+/**
+ * Created by Administrator on 2015/12/1.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.qiao.androidlab.lightreader.R;
+
+/**
+ * 获取图片后展示函数的界面
+ */
+public class ShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_layout);
+    }
+}
