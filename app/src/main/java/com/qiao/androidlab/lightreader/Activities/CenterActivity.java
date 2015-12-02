7fabcaafff3d00a1ca6a18fc68e8a8ca94eb3517
qiaoyunrui <2546521583@qq.com>
2015-12-02 11:45:46 +0800

    2015.12.1  重新连接以后

---
+++ app/src/main/java/com/qiao/androidlab/lightreader/Activities/CenterActivity.java
@@ -0,0 +1,18 @@
+package com.qiao.androidlab.lightreader.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.qiao.androidlab.lightreader.R;
+
+/**
+ * Created by Administrator on 2015/12/2.
+ */
+public class CenterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.center_layout);
+    }
+}
