67912a90e617ed2e7eb9694565ab24757f90dfbe
qiaoyunrui <2546521583@qq.com>
2016-01-03 15:47:44 +0800

    2016.1.3

--- app/src/main/java/com/qiao/androidlab/lightreader/Activities/MainActivity.java
+++ app/src/main/java/com/qiao/androidlab/lightreader/Activities/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -59,11 +60,16 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setTitle(R.string.mainTitle);
         getSupportActionBar().setDisplayShowCustomEnabled(true);
+        setHuillotine();
         recyclerView.setAdapter(adapter);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         intent = new Intent(MainActivity.this, CameraActivity.class);
+    }
+
+    private void setHuillotine() {
+        
     }
 
     /**
